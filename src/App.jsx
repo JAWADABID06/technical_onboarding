e61524a6a496489e3c0a501f5f856f1cc8815542
JAWADABID06 <157128743+JAWADABID06@users.noreply.github.com>
2024-03-20 19:55:02 +0000

    1st commit

--- src/App.jsx
+++ src/App.jsx
@@ -24,12 +24,12 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "JAWAD ABID",
+  title: "FRONTEND WEB DEVELOPER",
+  email: "jawadabid5292@gmail.com",
+  gitHub: "JAWADABID06",
+  instagram: "jawadabid50",
+  linkedIn: "jawadabid",
   medium: "",
   twitter: "microsoft",
   youTube: "Code",
